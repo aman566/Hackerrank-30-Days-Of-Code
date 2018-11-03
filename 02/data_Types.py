cb0729c6280bd105169c82ea2497b9a0c230c211
aman566 <32503192+aman566@users.noreply.github.com>
2018-11-03 19:22:21 +0530

    Create data_Types.py

---
+++ 02/data_Types.py
@@ -0,0 +1,13 @@
+
+# Declare second integer, double, and String variables.
+i1 = eval(input())
+d1 = eval(input())
+s1 = str(input())
+# Read and save an integer, double, and String to your variables.
+# Print the sum of both integer variables on a new line.
+print(i1+i)
+# Print the sum of the double variables on a new line.
+print(d+d1)
+# Concatenate and print the String variables on a new line
+print(s +s1)
+# The 's' variable above should be printed first.
